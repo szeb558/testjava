ab9fdad0da201f13190c4949002e0cce79b11a92
szebastian <szeb@users.noreply.github.com>
2020-09-11 03:09:16 -0300

    Versión inicial programa prueba.

---
+++ POO/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Persona p1 = new Persona("Sebastian",21);
+		p1.mostrarDatos();
+
+	}
+
+}
---
+++ POO/src/Persona.java
@@ -0,0 +1,19 @@
+
+public class Persona {
+
+	String nombre;
+	int edad;
+	//métodos
+	
+	//método constructor
+	public Persona(String nombre, int edad) {
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+	
+	public void mostrarDatos() {
+		System.out.println("El nombre es:" +nombre);
+		System.out.println("La edad es:" +edad);
+		
+	}
+}
